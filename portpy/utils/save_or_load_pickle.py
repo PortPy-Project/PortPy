e69eaf93b04d1a3120ddd7155634e60d827ed6f0
gourav3017 <gourav.jhanwar007@gmail.com>
2023-01-15 18:24:58 -0500

    Updated portpy documentation and reformat code

---
+++ portpy/utils/save_or_load_pickle.py
@@ -0,0 +1,90 @@
+import os
+import pickle
+from pathlib import Path
+
+
+def save_plan(my_plan, plan_name: str = None, path: str = None) -> None:
+    """
+
+    Save pickled file for plan object
+
+    :param plan_name: create the name of the pickled file of plan object. If none, it will save with the name as 'my_plan'
+    :param path: if path is set, plan object will be pickled and saved in path directory else it will save current project directory
+    :return: save pickled object of class Plan
+
+    :Example:
+    >>> my_plan.save_plan(plan_name='my_plan', path=r"path/to/save_plan")
+    """
+    if path is None:
+        path = os.path.join(Path(__file__).parents[2])
+    elif not os.path.exists(path):
+        os.makedirs(path)
+
+    if plan_name is None:
+        plan_name = 'my_plan'
+    with open(os.path.join(path, plan_name), 'wb') as pickle_file:
+        # pickle the dictionary and write it to file
+        pickle.dump(my_plan, pickle_file)
+
+
+def load_plan(plan_name: str = None, path: str = None):
+    """
+    Load pickle file of the plan object.
+
+    :param plan_name: plan_name of the object of class Plan. It None, it will try to look for plan name called 'my_plan'
+    :param path: if path is set, plan object will be load from path directory else current project directory
+    :return: load pickled object of class Plan
+
+    :Example:
+    >>> load_plan(plan_name='my_plan', path=r"path/for/loading_plan")
+    """
+    if path is None:
+        path = os.path.join(Path(__file__).parents[2])
+    elif not os.path.exists(path):
+        os.makedirs(path)
+
+    if plan_name is None:
+        plan_name = 'my_plan'
+    with open(os.path.join(path, plan_name), 'rb') as pickle_file:
+        my_plan = pickle.load(pickle_file)
+    return my_plan
+
+
+def load_optimal_sol(sol_name: str, path: str = None) -> dict:
+    """
+
+    Load optimal solution dictionary got from optimization
+
+    :param sol_name: name of the optimal solution to be loaded.
+    :param path: if path is set, plan object will be load from path directory else current directory
+    :return: load solution
+
+    :Example:
+    >>> sol = load_optimal_sol(sol_name='sol', path=r'path/for/loading_sol')
+    """
+    if path is None:
+        path = os.path.join(Path(__file__).parents[2])
+
+    with open(os.path.join(path, sol_name), 'rb') as pickle_file:
+        sol = pickle.load(pickle_file)
+    return sol
+
+
+def save_optimal_sol(sol: dict, sol_name: str, path: str = None) -> None:
+    """
+    Save the optimal solution dictionary from optimization
+
+    :param sol: optimal solution dictionary
+    :param sol_name: name of the optimal solution saved
+    :param path: if path is set, plan object will be load from path directory else current directory
+    :return: save pickled file of optimal solution dictionary
+
+    :Example:
+    >>> my_plan.save_optimal_sol(sol=sol, sol_name='sol', path=r'path/to/save_solution')
+    """
+    if path is None:
+        path = os.path.join(Path(__file__).parents[2])
+    elif not os.path.exists(path):
+        os.makedirs(path)
+    with open(os.path.join(path, sol_name), 'wb') as pickle_file:
+        pickle.dump(sol, pickle_file)
---
+++ portpy/utils/sol_change_inf_matrix.py
@@ -0,0 +1,24 @@
+from portpy.influence_matrix import InfluenceMatrix
+
+
+def sol_change_inf_matrix(sol: dict, inf_matrix: InfluenceMatrix) -> dict:
+    """
+    Create a new solution by changing the basis of current solution.
+    It will create a solution with same number of beamlets and voxels as inf_matrix
+
+
+    :param sol: solution for which influence matrix is changed
+    :param inf_matrix: object of class Influence matrix
+    :return: new solution dictionary having same number of beamlets and voxels as inf_matrix
+    """
+    new_sol = dict()
+    optimal_intensity = sol['inf_matrix'].fluence_1d_to_2d(fluence_1d=sol['optimal_intensity'])
+    new_sol['optimal_intensity'] = inf_matrix.fluence_2d_to_1d(optimal_intensity)
+
+    dose_3d = sol['inf_matrix'].dose_1d_to_3d(dose_1d=sol['dose_1d'])
+    new_sol['dose_1d'] = inf_matrix.dose_3d_to_1d(dose_3d=dose_3d)
+
+    new_sol['inf_matrix'] = inf_matrix
+    return new_sol
+
+
